7d15924f9f68d1c94bada5e15a08468fd24ba918
hasan <hasan@DESKTOP-01TTMBP>
2022-09-19 22:29:03 +0300

    loopDemo

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,34 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//for
+		for(int i=2;i<10;i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("For Döngü Bitti");
+	
+
+	int i =1 ;
+	//while
+	while (i<10) {
+		System.out.println(i);
+		i+=2;
+		}
+	System.out.println("While Döngü Bitti");
+	
+	
+	//Do-While
+	int j=100 ;
+	do {
+		System.out.println(j);
+		j+=2;
+	}while(j<10) ;
+		System.out.println("Do-While Döngü Bitti");
+		
+	}
+	
+	}
+
