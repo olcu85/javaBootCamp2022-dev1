da18f0a057db56f66ed710bf7f5b0cc662c95b8c
hasan <hasan@DESKTOP-01TTMBP>
2022-09-19 21:15:05 +0300

    datatypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,20 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double sayi = 12.5 ; 
+		sayi = -129;
+		char karakter = 'A' ; 
+		
+				
+				
+		
+		
+		boolean dogruMu=false;
+		
+
+	}
+
+}
