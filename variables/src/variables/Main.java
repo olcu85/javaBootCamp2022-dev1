c51636cacfa2498c0adf8fcaf7e68ee3719eeb0d
hasan <hasan@DESKTOP-01TTMBP>
2022-09-19 20:44:31 +0300

    variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int ogrenciSayisi = 12 ;
+    String mesaj = "öğrenci sayısı :" ; 
+    System.out.println(mesaj + ogrenciSayisi);
+    System.out.println(mesaj + ogrenciSayisi);
+    System.out.println("öğrenci sayım : " + ogrenciSayisi);
+    System.out.println("öğrenci sayım : " + ogrenciSayisi);
+    System.out.println("öğrenci sayım : " + ogrenciSayisi);
+	}
+
+}
